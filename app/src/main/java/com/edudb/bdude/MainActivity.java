6a39cb2b9f17c62f49673c909f5253c9ebb378d8
Nirel Didi <nirel.didi@gmail.com>
2020-03-18 22:20:30 +0200

    Firebase - Init first auth button

--- app/src/main/java/com/edudb/bdude/MainActivity.java
+++ app/src/main/java/com/edudb/bdude/MainActivity.java
@@ -1,14 +1,81 @@
 package com.edudb.bdude;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void signIn(View view) {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build(),
+//                                new AuthUI.IdpConfig.TwitterBuilder().build(),
+//                                new AuthUI.IdpConfig.MicrosoftBuilder().build(),
+//                                new AuthUI.IdpConfig.YahooBuilder().build(),
+//                                new AuthUI.IdpConfig.AppleBuilder().build(),
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.PhoneBuilder().build()
+                                //                       ,new AuthUI.IdpConfig.AnonymousBuilder().build()
+                        ))
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Successfully signed in!", Toast.LENGTH_SHORT).show();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar("Sign in cancelled");
+                    return;
+                }
+
+                if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar("No Internet connection");
+                    return;
+                }
+
+                showSnackbar("Unknown error");
+                Log.e("BDUDE", "Sign-in error: ", response.getError());
+            }
+        }
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
     }
 }
