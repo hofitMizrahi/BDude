437817434743130ec47916ef94ad709b91644bcf
MiHofit <hofitmizrahi92@gmail.com>
2020-03-21 14:20:57 +0200

    commit

--- app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/HelpRequestsListActivity.java
+++ app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/HelpRequestsListActivity.java
@@ -1,16 +1,7 @@
 package com.edudb.bdude.ui.flow.lobby.requests_list_screen.view;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.View;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.edudb.bdude.R;
 import com.edudb.bdude.application.BDudeApplication;
 import com.edudb.bdude.di.components.DaggerHelpRequestsListComponent;
@@ -20,17 +11,7 @@ import com.edudb.bdude.ui.base.BasePresenter;
 import com.edudb.bdude.ui.flow.lobby.requests_list_screen.contract.HelpRequestsListContract;
 import com.edudb.bdude.ui.flow.lobby.requests_list_screen.presenter.HelpRequestsListPresenter;
 import com.edudb.bdude.ui.flow.lobby.requests_list_screen.view.adapter.HelpRequestsRecyclerAdapter;
-import com.edudb.bdude.ui.flow.login.view.LoginActivity;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.Arrays;
-import java.util.Objects;
-
 import javax.inject.Inject;
-
 import butterknife.BindView;
 import butterknife.OnClick;
 
