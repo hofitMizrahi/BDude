e27876b6b1ab623852bb292dba018d74791cb7a7
Nirel Didi <nirel.didi@gmail.com>
2020-03-25 23:46:34 +0200

    Add random avatar

--- app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/adapter/HelpRequestViewHolder.java
+++ app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/adapter/HelpRequestViewHolder.java
@@ -7,20 +7,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.edudb.bdude.R;
-import com.edudb.bdude.db.modules.HelpRequest;
 import com.edudb.bdude.db.modules.Post;
 import com.edudb.bdude.general.utils.Utils;
 import com.edudb.bdude.interfaces.IExecutable;
 import com.edudb.bdude.location.LocationHelper;
 import com.google.android.gms.maps.model.LatLng;
-import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-
-import static java.lang.Math.round;
 
 public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
 
@@ -32,18 +29,19 @@ public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
 
     @BindView(R.id.destination)
     TextView mLocation;
-    @OnClick(R.id.root_item)
-    void onItemClicked(){
-        mListener.execute(mPost);
+
+    public HelpRequestViewHolder(@NonNull View itemView, IExecutable<Post> requestIExecutable) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+        mListener = requestIExecutable;
     }
 
     @BindView(R.id.avatar)
     ImageView mAvatar;
 
-    public HelpRequestViewHolder(@NonNull View itemView, IExecutable<Post> requestIExecutable) {
-        super(itemView);
-        ButterKnife.bind(this,itemView);
-        mListener = requestIExecutable;
+    @OnClick(R.id.root_item)
+    void onItemClicked() {
+        mListener.execute(mPost);
     }
 
     public void onBind(Post post) {
@@ -53,6 +51,6 @@ public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
         LatLng latLng = new LatLng(post.getGeoloc().getLat(), post.getGeoloc().getLng());
         String kmStr = Utils.round(LocationHelper.getDistance(latLng), 1) + " km";
         mLocation.setText(kmStr);
-        Picasso.get().load(post.getUserAvatar()).into(mAvatar);
+        Glide.with(mAvatar.getContext()).load("https://api.adorable.io/avatar/" + post.getId()).circleCrop().into(mAvatar);
     }
 }
