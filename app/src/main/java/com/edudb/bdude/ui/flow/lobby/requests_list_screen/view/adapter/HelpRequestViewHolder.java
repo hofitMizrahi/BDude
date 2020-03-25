9d38a157f2c371dd4eb6c1cb1af5f8cf11a2e5d5
MiHofit <hofitmizrahi92@gmail.com>
2020-03-26 00:04:03 +0200

    change ui

--- app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/adapter/HelpRequestViewHolder.java
+++ app/src/main/java/com/edudb/bdude/ui/flow/lobby/requests_list_screen/view/adapter/HelpRequestViewHolder.java
@@ -46,16 +46,9 @@ public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.avatar)
     ImageView mAvatar;
 
-<<<<<<< HEAD
-    HelpRequestViewHolder(@NonNull View itemView, IExecutable<Post> requestIExecutable) {
-        super(itemView);
-        ButterKnife.bind(this,itemView);
-        mListener = requestIExecutable;
-=======
     @OnClick(R.id.root_item)
     void onItemClicked() {
         mListener.execute(mPost);
->>>>>>> e27876b6b1ab623852bb292dba018d74791cb7a7
     }
 
     void onBind(Post post) {
@@ -65,9 +58,6 @@ public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
         LatLng latLng = new LatLng(post.getGeoloc().getLat(), post.getGeoloc().getLng());
         String kmStr = Utils.round(LocationHelper.getDistance(latLng), 1) + " km";
         mLocation.setText(kmStr);
-<<<<<<< HEAD
-
-        //TODO add string to xml && 1 hour
 
         int hours   = (int) ((mPost.getTimestamp() / (1000*60*60)) % 24);
         String fullTimeStr  = "";
@@ -77,11 +67,7 @@ public class HelpRequestViewHolder extends RecyclerView.ViewHolder {
         }else {
             fullTimeStr += "פורסם לפני " + hours + "שעות";
         }
-
         mHours.setText(fullTimeStr);
-        Picasso.get().load(post.getUserAvatar()).into(mAvatar);
-=======
         Glide.with(mAvatar.getContext()).load("https://api.adorable.io/avatar/" + post.getId()).circleCrop().into(mAvatar);
->>>>>>> e27876b6b1ab623852bb292dba018d74791cb7a7
     }
 }
